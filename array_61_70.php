c2c3cb2463cc7235750a440482e5a47498e75b8d
Ahmad Raza <raza.ataki@gmail.com>
2022-02-28 10:36:50 +0500

    Adding 61 to 70

--- array_61_70.php
+++ array_61_70.php
@@ -12,22 +12,75 @@ function pre_r(array $array)
 $array = array("The Matrix", "John Wick", "Kingsman", "The Witcher");
 $array2 = array("Science Fiction", "Action", "Thriller", "Season");
 
+$directory = "D:/Installations/laragon/www/php-built-in-fun/new-folder/sub-folder";
+
 /***************************** 51 mkdir() *******************************/
 
+// mkdir("D:/Installations/laragon/www/php-built-in-fun/new-folder/sub-folder", 0777, true);
+
 /***************************** 52 is_callable() *******************************/
+$print_array = 'pre_r';
+echo is_callable($print_array, false, $callable_name) . BR;
+echo $callable_name . BR;
 
 /***************************** 53 ltrim() *******************************/
-
+$string = " This is a message";
+var_dump($string);
+echo BR;
+var_dump(ltrim($string, " Th"));
+echo BR;
 /***************************** 54 ob_start() *******************************/
+ob_start();
+$hello = "Welcome to my awesome web site!";
+echo $hello;
+$content = ob_get_contents();
+ob_clean();
+echo $content . BR;
+
+ob_start('callback');
+function callback($buffer)
+{
+    return str_replace("C++", "PHP", $buffer);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<body>
+    <h1>C++ is a programming language</h1>
+</html>
+<?php
 
 /***************************** 55 round() *******************************/
+echo round(4.2) . BR;
+echo round(2.9857, PHP_ROUND_HALF_UP) . BR;
+echo round(-5.045, PHP_ROUND_HALF_DOWN) . BR;
+echo round(9.99, PHP_ROUND_HALF_ODD) . BR;
+echo round(9.99, PHP_ROUND_HALF_EVEN) . BR;
 
 /***************************** 56 fwrite() *******************************/
+// $handle = fopen('index.php', 'w');
+// fwrite($handle, "This is a good day to die");
 
 /***************************** 57 array_unique() *******************************/
+$animals = array("Lion", "Cat", "Zebra", "Bear", "Cat", "Lion", "Dog", "Dog");
+pre_r($animals);
+$unique_animals = array_unique($animals);
+pre_r($unique_animals);
 
 /***************************** 58 array_search() *******************************/
+$searched = array_search("Dog", $unique_animals, true);
+echo $unique_animals[$searched] . BR;
 
 /***************************** 59 reset() *******************************/
 
+echo current($unique_animals) . BR;
+echo next($unique_animals) . BR;
+echo next($unique_animals) . BR;
+echo next($unique_animals) . BR;
+echo next($unique_animals) . BR;
+echo reset($unique_animals) . BR;
+
 /***************************** 60 array_unshift() *******************************/
+array_unshift($unique_animals, "Panda") . BR;
+pre_r($unique_animals);
